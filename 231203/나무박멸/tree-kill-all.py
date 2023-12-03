b5a082883a888b3e89faebd9fd4240836a9ff6d5
th0214 <102945774+th0214@users.noreply.github.com>
2023-12-03 21:19:57 +0900

    Title: 나무박멸, Time: 385ms, Memory: 28MB, Status: Wrong Answer - CodeTree

--- 231203/나무박멸/tree-kill-all.py
+++ 231203/나무박멸/tree-kill-all.py
@@ -1,78 +1,105 @@
 from copy import deepcopy
-
 n, m, k, c = map(int, input().split())
 
-graph = [list(map(int, input().split())) for _ in range(n)]
-visited = [[0] * n for _ in range(n)]
+graph = [list(map(int,input().split())) for _ in range(n)]
 answer = 0
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
+visited = [[0] * n for _ in range(n)]
 
-ddx = [-1, 1, -1, 1]
-ddy = [1, 1, -1, -1]
+dx = [1,0,-1,0]
+dy = [0,1,0,-1]
+
+ddx = [-1, -1, 1, 1]
+ddy = [-1, 1, -1, 1]
+
 
 def grow():
     tmp = [[0] * n for _ in range(n)]
+
     for i in range(n):
         for j in range(n):
             if graph[i][j] > 0:
+
                 for a in range(4):
                     nx, ny = i + dx[a], j + dy[a]
                     if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] > 0:
                         tmp[i][j] += 1
+    
     for i in range(n):
         for j in range(n):
             graph[i][j] += tmp[i][j]
+                
 
 def seed():
     tmp = deepcopy(graph)
+
     for i in range(n):
         for j in range(n):
             if graph[i][j] > 0:
                 cnt = 0
+
                 for a in range(4):
                     nx, ny = i + dx[a], j + dy[a]
-                    if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] == 0 and visited[nx][ny] <= 0:
+                    if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] == 0 and visited[nx][ny] == 0:
                         cnt += 1
+                        
                 if cnt != 0:
                     tmp_cnt = graph[i][j] // cnt
+                    
                     for a in range(4):
                         nx, ny = i + dx[a], j + dy[a]
-                        if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] == 0 and visited[nx][ny] <= 0:
+                        if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] == 0 and visited[nx][ny] == 0:
                             tmp[nx][ny] += tmp_cnt
+    
     return tmp
 
-def killer(x, y):
-    global score, max_x, max_y
-    cnt = graph[x][y]
-    for i in range(4):
-        cur_x, cur_y = x, y
-        for j in range(k):
-            nx, ny = cur_x + ddx[i], cur_y + ddy[i]
-            if not (0 <= nx < n and 0 <= ny < n):
-                break
-            if graph[nx][ny] <= 0:
-                break
-            if graph[nx][ny] > 0:
-                cnt += graph[nx][ny]
-                cur_x, cur_y = nx, ny
-    if score < cnt:
-        score = cnt
-        max_x, max_y = x, y
 
-def kill_tree(x, y):
-    visited[x][y] += c
+def killer(x,y):
+    global score, max_x, max_y
+
+    value = graph[x][y] # 제초제로 죽일 수 있는 나무의 양
+
+    for p in range(4):
+        cur_x, cur_y = x, y
+        for _ in range(k):
+
+            mx = cur_x + ddx[p]
+            my = cur_y + ddy[p]
+
+            if not (0 <= mx < n and 0 <= my < n):
+                break
+
+            if graph[mx][my] <= 0:
+                break
+
+            if 1 <= graph[mx][my]:
+                value += graph[mx][my]
+                cur_x, cur_y = mx, my
+
+
+    if score < value:
+        max_x, max_y = x, y
+        score = value
+
+def kill_tree(x,y):
+    visited[x][y] = c
     graph[x][y] = 0
     for i in range(4):
         cur_x, cur_y = x, y
         for _ in range(k):
-            nx, ny = cur_x + ddx[i], cur_y + ddy[i]
+            nx, ny = x + ddx[i], y + ddy[i]
+            
             if not (0 <= nx < n and 0 <= ny < n):
                 break
+            
             if graph[nx][ny] == -1:
                 break
+            
+            if graph[nx][ny] == 0:
+                visited[nx][ny] = c
+                break
+
             if graph[nx][ny] > 0:
-                visited[nx][ny] += c
+                visited[nx][ny] = c
                 graph[nx][ny] = 0
                 cur_x, cur_y = nx, ny
 
@@ -85,14 +112,18 @@ def remove_killer():
 for _ in range(m):
     grow()
     graph = seed()
+
     score = 0
     max_x, max_y = 0, 0
     for i in range(n):
         for j in range(n):
             if graph[i][j] > 0:
-                killer(i, j)
-    kill_tree(max_x, max_y)
+                killer(i,j)
+    
     remove_killer()
-    answer += score
 
+    kill_tree(max_x, max_y)
+    
+
+    answer += score
 print(answer)
