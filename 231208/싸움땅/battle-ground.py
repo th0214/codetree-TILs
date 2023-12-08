ce86aae4b1a825553ea1292d51b6c984207efc7d
th0214 <102945774+th0214@users.noreply.github.com>
2023-12-08 18:04:08 +0900

    Title: 싸움땅, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231208/싸움땅/battle-ground.py
@@ -0,0 +1,146 @@
+n, m, k = map(int, input().split())
+
+graph = [list(map(int, input().split())) for _ in range(n)]
+person = [[[] for _ in range(n)] for _ in range(n)]
+
+for i in range(m):
+    x,y,d,s = map(int, input().split())
+    person[x-1][y-1].extend([d,s,i,0])
+
+dx = [-1,0,1,0]
+dy = [0,1,0,-1]
+score = [0 for _ in range(m)]
+
+def check_dir(i,j,nx,ny):
+    nnx, nny = nx+ dx[person[i][j][0]],  ny + dy[person[i][j][0]]
+
+    while len(person[nnx][nny]) != 0:
+        if len(person[nnx][nny]) >0:
+            person[i][j][0] = (person[i][j][0] + 1) % 4
+            nnx, nny = nx + dx[person[i][j][0]], ny + dy[person[i][j][0]]
+    
+    return nnx, nny
+
+def check_dir2(nx,ny):
+    nnx, nny = nx+ dx[person[nx][ny][0]],  ny + dy[person[nx][ny][0]]
+
+    while len(person[nnx][nny]) != 0:
+        if len(person[nnx][nny]) >0:
+            person[nx][ny][0] = (person[nx][ny][0] + 1) % 4
+            nnx, nny = nx + dx[person[nx][ny][0]], ny + dy[person[nx][ny][0]]
+    
+    return nnx, nny
+
+
+def check_gun(nx,ny):
+    if person[nx][ny][3] == 0 and graph[nx][ny] > 0:
+        person[nx][ny][3] = graph[nx][ny]
+        graph[nx][ny] = 0
+                    
+    elif person[nx][ny][3] > 0 and graph[nx][ny] > 0:
+        if person[nx][ny][3] < graph[nx][ny]:
+            person[nx][ny][3] = graph[nx][ny]
+            graph[nx][ny] = person[nx][ny][3]
+                    
+def rotate_dir(d):
+    if d == 0:
+        return 2
+    elif d == 1:
+        return 3
+    elif d == 2:
+        return 0
+    elif d == 3:
+        return 1
+
+def p_move():
+    number = 0
+    while number != m:
+        moved = False
+        for i in range(n):
+            if moved:
+                    break
+            for j in range(n):
+                if moved:
+                        break
+                if len(person[i][j]) > 0:
+                    if person[i][j][2] == number:
+                        nx, ny = i + dx[person[i][j][0]], j + dy[person[i][j][0]]
+                        if 0 <= nx < n and 0 <= ny < n:
+                            if len(person[nx][ny]) == 0:
+                                person[nx][ny] = person[i][j]
+                                person[i][j] = []
+                                moved = True
+                                check_gun(nx,ny)
+
+                            else:
+                                f1 = person[nx][ny][1] + person[nx][ny][3]
+                                f2 = person[i][j][1] + person[i][j][3]
+
+                                if f1 > f2:
+                                    score[person[nx][ny][2]] += f1-f2
+
+                                    if person[i][j][3] > 0:
+                                        graph[nx][ny].append(person[i][j][3])
+
+                                    nnx,nny = check_dir(i,j,nx,ny)
+                                    person[nnx][nny] = person[i][j]
+                                    person[i][j] = []
+                                    moved = True
+                                    check_gun(nnx,nny)
+                                else:
+                                    score[person[i][j][2]] += f2 - f1
+
+                                    if person[nx][ny][3] > 0:
+                                        graph[nx][ny].append(person[nx][ny][3])
+                                    
+                                    nnx,nny = check_dir2(nx,ny)
+                                    person[nnx][nny] = person[nx][ny]
+                                    person[nx][ny] = person[i][j]
+                                    moved = True
+                                    check_gun(nnx,nny)
+                            
+                            
+                        else:
+                            person[i][j][0] = rotate_dir(person[i][j][0])
+                            nx, ny = i + dx[person[i][j][0]], j + dy[person[i][j][0]]
+                            if len(person[nx][ny]) == 0:
+                                person[nx][ny] = person[i][j]
+                                person[i][j] = []
+                                moved = True
+                                check_gun(nx,ny)
+
+                            else:
+                                f1 = person[nx][ny][1] + person[nx][ny][3]
+                                f2 = person[i][j][1] + person[i][j][3]
+
+                                if f1 > f2:
+                                    score[person[nx][ny][2]] += f1-f2
+
+                                    if person[i][j][3] > 0:
+                                        graph[nx][ny].append(person[i][j][3])
+
+                                    nnx,nny = check_dir(i,j,nx,ny)
+                                    person[nnx][nny] = person[i][j]
+                                    person[i][j] = []
+                                    moved = True
+                                    check_gun(nnx,nny)
+                                else:
+                                    score[person[i][j][2]] += f2 - f1
+
+                                    if person[nx][ny][3] > 0:
+                                        graph[nx][ny].append(person[nx][ny][3])
+                                    
+                                    nnx,nny = check_dir2(nx,ny)
+                                    person[nnx][nny] = person[nx][ny]
+                                    person[nx][ny] = person[i][j]
+                                    moved = True
+                                    check_gun(nnx,nny)
+                            
+                            
+        number += 1
+
+
+for _ in range(k):
+    p_move()
+
+print(*score)
