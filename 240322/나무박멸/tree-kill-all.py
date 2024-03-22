75b29a192e66b3eacd7049f2b5a584155b60ef10
th0214 <102945774+th0214@users.noreply.github.com>
2024-03-22 14:42:18 +0900

    Title: 나무박멸, Time: 140ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240322/나무박멸/tree-kill-all.py
+++ 240322/나무박멸/tree-kill-all.py
@@ -1,5 +1,5 @@
 from collections import deque
-from copy import deepcopy
+
 n, m, k, c = map(int, input().split())
 
 graph = [list(map(int, input().split())) for _ in range(n)]
@@ -7,64 +7,65 @@ sp = [[0] * n for _ in range(n)]
 result = 0
 
 def grow(x,y):
-    tmp = [[0] * n for _ in range(n)]
     dx = [-1,0,1,0]
     dy = [0,1,0,-1]
 
+    q = deque()
+    q.append((x,y))
     cnt = 0
-    
-    for k in range(4):
-        nx, ny = x + dx[k], y + dy[k]
+    while q:
+        i,j = q.popleft()
 
-        if 0 <= nx < n and 0 <= ny < n:
-            if graph[nx][ny] > 0:
-                tmp[x][y] += 1
+        for k in range(4):
+            ni, nj = i + dx[k], j + dy[k]
 
-    for i in range(n):
-        for j in range(n):
-            graph[i][j] += tmp[i][j]
+            if 0 <= ni < n and 0 <= nj < n:
+                if graph[ni][nj] > 0:
+                    cnt += 1
 
-def bunsik():
-    tmp = deepcopy(graph)
+    graph[i][j] += cnt 
+
+def bunsik(i,j,tmp):
     dx = [-1,0,1,0]
     dy = [0,1,0,-1]
+    location = []
+    q = deque()
+    q.append((i,j))
+    cnt = 0
+    while q:
+        i,j = q.popleft()
 
-    for i in range(n):
-        for j in range(n):
-            if graph[i][j] > 0:
-                cnt = 0
+        for k in range(4):
+            ni, nj = i + dx[k], j + dy[k]
 
-                for a in range(4):
-                    nx, ny = i + dx[a], j + dy[a]
-                    if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] == 0 and sp[nx][ny] == 0:
-                        cnt += 1
-                        
-                if cnt != 0:
-                    tmp_cnt = graph[i][j] // cnt
-                    
-                    for a in range(4):
-                        nx, ny = i + dx[a], j + dy[a]
-                        if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] == 0 and sp[nx][ny] == 0:
-                            tmp[nx][ny] += tmp_cnt
-    
-    return tmp
+            if 0 <= ni < n and 0 <= nj < n:
+                if graph[ni][nj] == 0 and sp[ni][nj] == 0:
+                    cnt += 1
+                    location.append([ni,nj])
+    for x,y in location:
+        tmp[x][y] += (graph[i][j] // cnt)
 
 def check_kill(i,j,killer):
     dx = [-1,-1,1,1]
     dy = [-1,1,-1,1]
 
+    q = deque()
+    q.append((i,j))
     cnt = graph[i][j]
-    x,y = i,j
-    for i in range(4):
-        for j in range(1,k+1):
-            nx = x + dx[i] * j
-            ny = y + dy[i] * j
 
-            if 0 <= nx < n and 0 <= ny < n:
-                if graph[nx][ny] > 0:
-                    cnt += graph[nx][ny]
-                if graph[nx][ny] <= 0:
-                    break
+    while q:
+        x,y = q.popleft()
+
+        for j in range(4):
+            for i in range(1,k+1):
+                nx = x + dx[j] * i
+                ny = y + dy[j] * i
+
+                if 0 <= nx < n and 0 <= ny < n:
+                    if graph[nx][ny] > 0:
+                        cnt += graph[nx][ny]
+                    if graph[nx][ny] == -1 or graph[nx][ny] == 0:
+                        break
     
     if killer[0][2] < cnt:
         killer.pop()
@@ -73,24 +74,28 @@ def check_kill(i,j,killer):
 def kill(killer):
     dx = [-1,-1,1,1]
     dy = [-1,1,-1,1]
-    x,y = killer[0][0], killer[0][1]
+    i,j = killer[0][0], killer[0][1]
 
-    sp[x][y] = c+1
+    q = deque()
+    q.append((i,j))
+    sp[i][j] = c+1
 
-    for i in range(4):
-        for j in range(1, k+1):
-            nx = x + (dx[i] * j)
-            ny = y + (dy[i] * j)
+    while q:
+        x,y = q.popleft()
 
-            if 0 <= nx < n and 0 <= ny < n:
-                if graph[nx][ny] > 0:
-                    sp[nx][ny] = c+1
-                if graph[nx][ny] == -1:
-                    break
-                if graph[nx][ny] == 0:
-                    sp[nx][ny] = c+1
-                    break
+        for i in range(4):
+            for j in range(1,k+1):
+                nx = x + (dx[i] * j)
+                ny = y + (dy[i] * j)
 
+                if 0 <= nx < n and 0 <= ny < n:
+                    if graph[nx][ny] > 0:
+                        sp[nx][ny] = c+1
+                    if graph[nx][ny] == -1:
+                        break
+                    if graph[nx][ny] == 0:
+                        sp[nx][ny] = c+1
+                        break
     for i in range(n):
         for j in range(n):
             if sp[i][j] > 0:
@@ -100,18 +105,27 @@ def minus():
     for i in range(n):
         for j in range(n):
             if sp[i][j] > 0:
-                sp[i][j] -= 1
+                sp[i][j] -= 0
 
 for _ in range(m):
+    tmp = [[0] * n for _ in range(n)]
     killer = [[0,0,0]]
+    sp = [[0] * n for _ in range(n)]
 
     for i in range(n):
         for j in range(n):
             if graph[i][j] > 0:
                 grow(i,j)
     
-    graph = bunsik()
-
+    for i in range(n):
+        for j in range(n):
+            if graph[i][j] > 0:
+                bunsik(i,j,tmp)
+    
+    for i in range(n):
+        for j in range(n):
+            graph[i][j] += tmp[i][j]
+    
     for i in range(n):
         for j in range(n):
             if graph[i][j] > 0:
