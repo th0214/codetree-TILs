93b2535ba754d7739e1bef82e84c62ae00a83c1a
th0214 <102945774+th0214@users.noreply.github.com>
2024-04-07 21:04:20 +0900

    Title: 포탑 부수기, Time: 145ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240407/포탑 부수기/destroy-the-turret.py
+++ 240407/포탑 부수기/destroy-the-turret.py
@@ -1,94 +1,92 @@
 from collections import deque
-N, M, K = map(int, input().split())
-
+N,M,K = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(N)]
+attack_life = [[0] * M for _ in range(N)]
 
 l_x = [0,1,0,-1]
 l_y = [1,0,-1,0]
 
-pok_x = [0,-1,-1,-1,0,1,1,1]
-pok_y = [-1,-1,0,1,1,1,0,-1]
-attack_exper = [[0] * M for _ in range(N)]
-# print(graph)
+p_x = [0,-1,-1,-1,0,1,1,1]
+p_y = [-1,-1,0,1,1,1,0,-1]
 
-def attacker_c(now):
+def choose_attack(now):
+    tmp = []
     min_val = 1e9
 
-    tmp = []
     for i in range(N):
         for j in range(M):
-            if graph[i][j] <= min_val and graph[i][j] !=0:
+            if graph[i][j] != 0 and min_val >= graph[i][j]:
                 min_val = graph[i][j]
-                tmp.append((i,j, i+j, attack_exper[i][j], graph[i][j]))
+                tmp.append((graph[i][j],attack_life[i][j],i+j,i,j))
     
-    tmp.sort(key=lambda x: (x[4],-x[3],-x[2],-x[1]))
-    attack_exper[tmp[0][0]][tmp[0][1]] = now
-    graph[tmp[0][0]][tmp[0][1]] += (N+M)
+    tmp.sort(key=lambda x:(x[0],x[1],-x[2],-x[4]))
+    attack_life[tmp[0][3]][tmp[0][4]] = now
+    graph[tmp[0][3]][tmp[0][4]] += (N+M)
 
-    return tmp[0][0], tmp[0][1]
+    return tmp[0][3], tmp[0][4]
 
-
-def attack(a,b,now):
+def attack(a,b):
+    tmp = []
     q = deque()
     q.append((a,b,[]))
     max_val = -1e9
-    
-    tmp = []
+
     for i in range(N):
         for j in range(M):
-            if graph[i][j] != 0 and (i,j) != (a,b) and graph[i][j] >= max_val:
-                    max_val = graph[i][j]
-                    tmp.append((i,j, i+j, attack_exper[i][j], graph[i][j]))
-    
-    if len(tmp):
-        tmp.sort(key=lambda x:(-x[4],x[3],x[2],x[1]))
-        a_x, a_y = tmp[0][0], tmp[0][1]
+            if graph[i][j] != 0 and (i,j) != (a,b) and max_val <= graph[i][j]:
+                max_val = graph[i][j]
+                tmp.append((graph[i][j],attack_life[i][j],i+j,i,j))
 
-        lazer_attack = False
-        route = []
-        route_length = 1e9
-        visited = [[0] * M for _ in range(N)]
-        visited[a][b] = 1
+    tmp.sort(key=lambda x:(-x[0],x[1],x[2],x[4]))
+    at_x, at_y = tmp[0][3], tmp[0][4]
 
-        while q:
-            x,y,arr = q.popleft()
+    # 레이저 공격 or 포탄 공격
 
-            if x == a_x and y == a_y:
-                if route_length > len(arr):
-                    route_length = len(arr)
+    can_lazer = False
+    route = []
+    route_length = 1e9
+    visited = [[0] * M for _ in range(N)]
+    visited[a][b] = 1
+
+    while q:
+        x,y,arr = q.popleft()
+
+        for i in range(4):
+            nx, ny = (x + l_x[i]) % N, (y + l_y[i]) % M
+
+            if (nx,ny) == (at_x,at_y):
+                if route_length > len(route):
+                    route_length = len(route)
                     route.extend(arr)
 
-            for i in range(4):
-                nx, ny = (x + l_x[i]) % N, (y + l_y[i]) % M
-
-                if visited[nx][ny] == 0 and graph[nx][ny] > 0:
-                    visited[nx][ny] = 1
-                    q.append((nx,ny,arr+[(nx,ny)]))
-        
+            if graph[nx][ny] != 0 and visited[nx][ny] == 0:
+                visited[nx][ny] = 1
+                q.append((nx,ny,arr+[(nx,ny)]))
     
-        if len(route):
-            lazer_attack = True
-        
-        if lazer_attack:
-            for x,y in route:
-                if x == a_x and y == a_y:
-                    graph[x][y] -= graph[a][b]
-                    attack_exper[x][y] = now
-                else:
-                    graph[x][y] -= graph[a][b] // 2
-                    attack_exper[x][y] = now
-        else:
+    if len(route):
+        can_lazer = True
+    
+    if can_lazer:
+        graph[at_x][at_y] -= graph[a][b]
+        attack_life[at_x][at_y] = now
 
-            graph[a_x][a_y] -= graph[a][b]
-            attack_exper[a_x][a_y] = now
-            for i in range(8):
-                nx, ny = (a_x + pok_x[i])%N, (a_y + pok_y[i])%M
+        for i,j in route:
+            graph[i][j] -= (graph[a][b] // 2)
+            attack_life[i][j] = now
+    
+    else:
 
-                if graph[nx][ny] > 0 and (nx, ny) != (a,b):
-                    graph[nx][ny] -= (graph[a][b] // 2)
-                    attack_exper[nx][ny] = now
+        graph[at_x][at_y] -= graph[a][b]
+        attack_life[at_x][at_y] = now
 
-def check_zero():
+        for i in range(8):
+            g_x, g_y = (at_x + p_x[i]) % N, (at_y + p_y[i]) % M
+
+            if graph[g_x][g_y] > 0 and (g_x,g_y) != (a,b):
+                graph[g_x][g_y] -= (graph[a][b] // 2)
+                attack_life[g_x][g_y] = now
+
+def check():
     for i in range(N):
         for j in range(M):
             if graph[i][j] <= 0:
@@ -97,35 +95,31 @@ def check_zero():
 def cure(now):
     for i in range(N):
         for j in range(M):
-            if attack_exper[i][j] < now and graph[i][j] != 0:
+            if graph[i][j] < now:
                 graph[i][j] += 1
 
-
-for now in range(1,K+1):
+def give_up():
     cnt = 0
-
     for i in range(N):
         for j in range(M):
             if graph[i][j] > 0:
                 cnt += 1
+    return cnt
+
+for now in range(1,K+1):
     
-    if cnt == 1:
+    a_x, a_y = choose_attack(now)
+    attack(a_x,a_y)
+    check()
+    cure(now)
+
+    if give_up() == 1:
         break
 
-    x,y = attacker_c(now)
-    # if now == 1:
-    #     print(graph)
-    attack(x,y, now)
-    # if now == 1:
-    #     print(graph)
-    check_zero()
-    cure(now)
-    # if now == 1:
-    #     print(graph)
-    
-result = 0
+result = -1e9
 
 for i in graph:
     if result < max(i):
         result = max(i)
+
 print(result)
