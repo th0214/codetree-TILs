f0d92ed20f9e79316de45f86002229d88115b791
th0214 <102945774+th0214@users.noreply.github.com>
2024-04-08 14:11:16 +0900

    Title: 코드트리 빵, Time: 245ms, Memory: 26MB, Status: Passed - CodeTree

--- 240408/코드트리 빵/codetree-mon-bread.py
+++ 240408/코드트리 빵/codetree-mon-bread.py
@@ -1,115 +1,102 @@
 from collections import deque
+n,m = map(int,input().split())
+board=[]
+for _ in range(n):
+    board.append(list(map(int,input().split())))
+lock=[[0]*n for _ in range(n)]
 
-n,m = map(int, input().split())
+people=[(-1,-1) for _ in range(m+1)]
+conv=[(-1,-1)]
 
-graph = [list(map(int, input().split())) for _ in range(n)]
-d_graph = [[False] * n for _ in range(n)]
-h_graph = [[False] * n for _ in range(n)]
+visited=[[0]*n for _ in range(n)]
+step = [[0] * n for _ in range(n)]
 
-p_l = [[0]]
-n_l = [[0]]
+# 편의점(도착지) 정보 위치
+for _ in range(m):
+    cy,cx = map(int,input().split())
+    conv.append((cy-1,cx-1))
 
-basecamp = [False] * (m+1)
-arrive = [True] * (m+1)
+def isAllPassed():
+    for i in range(1,m+1):
+        if people[i]!=conv[i]:
+            return False
+    return True
 
-dx = [-1,0,0,1]
-dy = [0,-1,1,0]
+d=[(-1,0),(0,-1),(0,1),(1,0)] # ↑, ←, →, ↓ 의 우선 순위
 
-for i in range(1,m+1):
-    x,y = map(int, input().split())
-    p_l.append([x-1,y-1])
-    n_l.append([x-1,y-1])
-
-def move_basecamp(t):
-    tmp = []
+def bfs(sy,sx): # bfs 시작 좌표를 매개변수로 받아옴
+    # visited, step 값을 전부 초기화합니다.
+    for i in range(n):
+        for j in range(n):
+            visited[i][j] = 0
+            step[i][j] = 0
     q = deque()
-    q.append((p_l[t][0],p_l[t][1],0))
-    visited = [[0] * n for _ in range(n)]
-    visited[p_l[t][0]][p_l[t][1]] = 1
-
+    q.append((sy, sx))
+    visited[sy][sx] = 1
     while q:
-        x,y, cnt = q.popleft()
+        y, x = q.popleft()
+        for dy, dx in d:
+            Y = y + dy
+            X = x + dx
+            if 0 <= Y < n and 0 <= X < n and not visited[Y][X] and lock[Y][X] == 0:
+                visited[Y][X] = 1
+                step[Y][X] = step[y][x] + 1
+                q.append((Y, X))
 
-        for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
-
-            if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] == 1 and h_graph[nx][ny] == False and d_graph[nx][ny] == False:
-                if (cnt, nx ,ny) in tmp:
-                    pass
-                else:
-                    tmp.append((cnt,nx,ny))
-
-            if 0 <= nx < n and 0 <= ny < n and visited[nx][ny] == 0 and d_graph[nx][ny] == False and h_graph[nx][ny] == False:
-                visited[nx][ny] = 1
-                q.append((nx,ny,cnt+1))
-
-    tmp.sort(key=lambda x:(x[0],x[1],x[2]))
-    # if t == 4:
-    #     print(tmp)
-    n_l[time] = [tmp[0][1], tmp[0][2]]
-    h_graph[tmp[0][1]][tmp[0][2]] = True
-    basecamp[time] = True
-
-def go_conv():
-    num = []
-    
-    for i in range(1, m+1):
-
-        if basecamp[i] == True and d_graph[p_l[i][0]][p_l[i][1]] == False:
-            num.append(i)
-    
-    if len(num):
-        for i in num:
-            tmp = []
-            q = deque()
-            q.append((n_l[i][0],n_l[i][1],0,-1))
-            visited = [[0] * n for _ in range(n)]
-            visited[n_l[i][0]][n_l[i][1]] = 1
-            
-            while q:
-                x,y,cnt,direction = q.popleft()
-
-                if x == p_l[i][0] and y == p_l[i][1]:
-                    tmp.append((cnt,n_l[i][0],n_l[i][1],direction))
-
-                for j in range(4):
-                    nx, ny = x + dx[j], y + dy[j]
-
-                    if 0 <= nx < n and 0 <= ny < n and d_graph[nx][ny] == False and h_graph[nx][ny] == False and visited[nx][ny] == 0:
-                        visited[nx][ny] = 1
-                        if cnt == 0:
-                            q.append((nx,ny,cnt+1,j))
-                        else:
-                            q.append((nx,ny,cnt+1,direction)) 
-                        
-            tmp.sort(key=lambda x:x[0])
-            f_x, f_y = tmp[0][1] + dx[tmp[0][3]], tmp[0][2] + dy[tmp[0][3]]
-
-            n_l[i] = [f_x,f_y]
-
-            if (f_x,f_y) == (p_l[i][0],p_l[i][1]):
-                arrive[i] = False
-                d_graph[f_x][f_y] = True
+def lock_board():
+    for i in range(1,m+1):
+        if people[i]==conv[i]:
+            py,px=people[i]
+            lock[py][px]=1
 
 
-time = 0
 
-while True:
-    if any(arrive[1:]) == False:
+def enterBaseCamp(time):
+    global m,n,d,board,people
+    cy,cx=conv[time]
+    bfs(cy, cx)
+    dist = 1e9  # 최단거리값
+    by, bx = -1, -1
+    for i in range(n):
+        for j in range(n):
+            # 방문 가능한 베이스 캠프 중 거리가 가장 가까운 위치를 찾기
+            if visited[i][j] and board[i][j] == 1 and dist > step[i][j]:
+                dist = step[i][j]
+                by,bx = i, j
+    people[time]=(by,bx)
+    lock[by][bx]=1
+    # print(baseCamp[time],people[time])
+
+
+
+
+def simulate():
+    for i in range(1,m+1):
+        # 아직 격자 밖에 있는 사람이거나 이미 편의점에 도착한 사람이라면 패스
+        if people[i] == (-1,-1) or people[i] == conv[i]:
+            continue
+        cy,cx=conv[i]
+        bfs(cy,cx)
+        py,px = people[i]
+        dist = 1e9
+        ty, tx = -1, -1  # tmp 좌표
+        for dy, dx in d:
+            PY = py + dy
+            PX = px + dx
+            if 0 <= PY < n and 0 <= PX < n and visited[PY][PX] and dist > step[PY][PX]:
+                dist = step[PY][PX]
+                ty, tx = PY, PX
+        people[i] = (ty, tx)
+    lock_board()
+    if time>m:
+        return
+    enterBaseCamp(time) 
+    # print('1턴')
+time=0
+while 1:
+    time+=1
+    simulate()
+    # 전부 이동이 끝났다면 종료
+    if isAllPassed():
         break
-
-    time += 1
-
-    if any(basecamp) == True:
-        go_conv()
-
-    if time <= m:
-        move_basecamp(time)
-
-# print(d_graph)
-# print(p_l)
-# print(n_l)
-
 print(time)
-
-# print(time)
